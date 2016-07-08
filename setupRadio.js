9e838dc92ed2dbf92930d1baa971b7ab7a642b81
KyleNehman <felreava@gmail.com>
2016-07-08 17:40:16 -0400

    Added radio button support, with known bug

--- setupRadio.js
+++ setupRadio.js
@@ -1,26 +1,26 @@
-function setupRadio(x, y, radioOptions, verticalFlag) {
+function setupRadio(x, y, radioOptions, verticalFlag, defaultOption) {
   var BUTTON_WIDTH = 30;
   var labelFontSize = floor(height / 75);
   var thisRadio = createRadio();
   var longestOptionLength = 0;
   var radioWidth;
 
-  for (var optionIndex in radioOptions) {
-    var option = radioOptions[optionIndex];
+  for (var i = 0; i < radioOptions.length; i++) {
+    var option = radioOptions[i];
     var optionLength = textWidth(option);
 
-    thisRadio.option(option, optionIndex)
+    thisRadio.option(option, i);
     if (optionLength > longestOptionLength) {
       longestOptionLength = optionLength;
     }
   }
   thisRadio.position(x, y);
   if (verticalFlag) {
-    text(longestOptionLength, 400, 400);
     radioWidth = BUTTON_WIDTH + longestOptionLength;
     thisRadio.style('width', radioWidth + 'px');
   }
 
+  thisRadio.value(defaultOption);
   thisRadio.style('font-size', labelFontSize + 'pt');
   return thisRadio;
 }
