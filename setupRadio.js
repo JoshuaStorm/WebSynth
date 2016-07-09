1e02b5482d676ab84f40b3e2cc3755865153c653
KyleNehman <felreava@gmail.com>
2016-07-09 10:44:42 -0400

    Editted setupRadio function in order to pass PR review

--- setupRadio.js
+++ setupRadio.js
@@ -1,30 +1,31 @@
-function setupRadio(x, y, radioOptions, verticalFlag, defaultOption) {
-  var BUTTON_WIDTH = 30;
+// Note: Due to a bug with how radio values are handled,
+// This Radio object's values will be OFF BY ONE. This
+// means that the first button will have value of 1, not 0
+
+function setupRadio(x, y, labelsArray, verticalFlag, defaultButton) {
+  var BUTTON_WIDTH = 30; // In pixels
   var labelFontSize = floor(height / 75);
   var thisRadio = createRadio();
-  var longestOptionLength = 0;
+  var longestLabelLength = 0;
   var radioWidth;
 
-  for (var i = 0; i < radioOptions.length; i++) {
-    var option = radioOptions[i];
-    var optionLength = textWidth(option);
+  for (var i = 0; i < labelsArray.length; i++) {
+    var label = labelsArray[i];
+    var labelLength = textWidth(label);
 
-    thisRadio.option(option, i);
-    if (optionLength > longestOptionLength) {
-      longestOptionLength = optionLength;
+    thisRadio.option(label, i + 1);
+    if (labelLength > longestLabelLength) {
+      longestLabelLength = labelLength;
     }
   }
   thisRadio.position(x, y);
   if (verticalFlag) {
-    radioWidth = BUTTON_WIDTH + longestOptionLength;
+    radioWidth = BUTTON_WIDTH + longestLabelLength;
     thisRadio.style('width', radioWidth + 'px');
   }
 
-  if(defaultOption) {
-    thisRadio.value(defaultOption);
-  } else {
-    thisRadio.value(1);
-}
+  thisRadio.value(defaultButton || 1);
+
   thisRadio.style('font-size', labelFontSize + 'pt');
   return thisRadio;
 }
